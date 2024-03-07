96e510338d523077da1a929f14b6f815d81be1b0
Sarvesh Thiruppathi <sarveshthiruppathi@gmail.com>
2024-03-06 19:45:05 -0800

    Implemented a test API endpoint "/validUsername" - takes username as input and returns jsonified 0/1

--- Backend/main.go
+++ Backend/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"context"
+	"net/http"
 	"fmt"
-	"log"
-	"os"
-	"time"
+	"encoding/json"
 
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
+	// "context"
+	// "log"
+	// "os"
+	// "time"
+	// "github.com/joho/godotenv"
+	// "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	// "go.mongodb.org/mongo-driver/mongo"
+	// "go.mongodb.org/mongo-driver/mongo/options"
+	// "go.mongodb.org/mongo-driver/mongo/readpref"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -24,6 +26,7 @@ type User struct {
 	Password   string             `bson:"password"`
 	NumFriends int                `bson:"numFriends"`
 	Friends    []string           `bson:"friends"` // list of friend usernames
+	FriendRequests []string       `bson:"friendRequests"` // list of friend requests
 	SleepStats []SleepStatistic   `bson:"sleepStats"`
 }
 
@@ -45,90 +48,38 @@ func CheckPasswordHash(password, hash string) bool {
 	return err == nil
 }
 
+func UsernameAvailable(w http.ResponseWriter, r *http.Request) {
+	// Get the username from the request
+	username := r.URL.Query().Get("username")
+
+	w.Header().Set("Content-Type", "application/json")
+	response := map[string]int{"result": 0} //Default response is no username available, updates to 1 if username is available
+
+	//Check is username is already in MongoDB 
+	
+
+	if username == "" {
+		http.Error(w, "No username provided", http.StatusBadRequest)
+		return
+	} else if username == "test1" { //TODO: Implement logic to check if username is available here
+        response["result"] = 1
+	} 
+		
+	// Send the response
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func main() {
-	// load environment variables
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
-	// get the environment variable for the connection string
-	connectString := os.Getenv("MONGODB_CONNECT_STRING")
+	http.HandleFunc("/validUsername", UsernameAvailable)
 
-	// if the environment variable is not set, then exit the program
-	if connectString == "" {
-		log.Fatal("MONGODB_CONNECT_STRING environment variable not set")
-	}
+    // Start the HTTP server on port 8080 and log any errors
+    fmt.Println("Server is running on port 8080")
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Println("Error starting server: ", err)
+    }
 
-	// create a new client (user in our case)
-	client, err := mongo.NewClient(options.Client().ApplyURI(connectString))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
-
-	if err := client.Ping(ctx, readpref.Primary()); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Successfully connected and pinged MongoDB!")
-
-	databases, err := client.ListDatabaseNames(ctx, bson.M{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(databases)
-
-	// get the users collection
-	coll := client.Database("users_db").Collection("users")
-	docs := []interface{}{
-		User{
-			Name:       "Carson",
-			Username:   "carpet",
-			NumFriends: 3,
-			Friends:    []string{"jinnypooh", "sebas", "rist"},
-			SleepStats: []SleepStatistic{
-				{
-					Date:       primitive.NewDateTimeFromTime(time.Now()),
-					HoursSlept: 8.5,
-				},
-			},
-		},
-		User{
-			Name:       "Sebastian",
-			Username:   "sebas",
-			NumFriends: 2,
-			Friends:    []string{"carpet", "jinnypooh"},
-			SleepStats: []SleepStatistic{
-				{
-					Date:       primitive.NewDateTimeFromTime(time.Now()),
-					HoursSlept: 7.5,
-				},
-			},
-		},
-		User{
-			Name:       "Jinny",
-			Username:   "jinnypooh",
-			NumFriends: 2,
-			Friends:    []string{"carpet", "sebas"},
-			SleepStats: []SleepStatistic{
-				{
-					Date:       primitive.NewDateTimeFromTime(time.Now()),
-					HoursSlept: 6.5,
-				},
-			},
-		},
-	}
-
-	// insert the documents into the collection
-	result, err := coll.InsertMany(context.TODO(), docs)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Inserted %d documents into collection\n", len(result.InsertedIDs))
 }
