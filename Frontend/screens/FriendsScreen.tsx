97752d48d94f72e5dada3f8179097b4b9e3b2b80
Carson274 <carsonsecrest@gmail.com>
2024-03-10 23:58:25 -0700

    Created tab navigation to navigate through the friends, pending, and add tabs in the friends screen

--- Frontend/screens/FriendsScreen.tsx
+++ Frontend/screens/FriendsScreen.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import FriendsListComponent from '../components/FriendsListComponent';
+import FriendsPendingComponent from '../components/FriendsPendingComponent';
+import FriendsAddComponent from '../components/FriendsAddComponent';
 
-const FriendsScreen = () => {
-    return (
-        <View style={styles.view}>
-            <Text style={styles.text}>Friends Screen</Text>
-        </View>
-    );
+const Tab = createMaterialTopTabNavigator();
+
+const FriendsScreen = ({ username }) => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="FriendsList"
+        tabBarPosition="top"
+        screenOptions={{
+          tabBarActiveTintColor: 'white',
+          tabBarLabelStyle: { fontSize: 16, textTransform: 'none'  },
+          tabBarStyle: { backgroundColor: 'transparent' },
+          tabBarIndicatorStyle: { backgroundColor: '#666CFF' },
+        }}
+      >
+        <Tab.Screen name="Friends" component={FriendsListComponent} initialParams={{ username: username }} />
+        <Tab.Screen name="Pending" component={FriendsPendingComponent} initialParams={{ username: username }} />
+        <Tab.Screen name="Add" component={FriendsAddComponent} initialParams={{ username: username }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 };
-
-const styles = StyleSheet.create({
-    view: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        color: 'white',
-        fontSize: 36,
-    }
-});
 
 export default FriendsScreen;
